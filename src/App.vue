a3e647a2056642be415fa18ebb961ea8b26ee9bb
Devin Beliveau <devinmarieb@gmail.com>
2020-09-23 17:19:21 +0200

    Add Vue, set up API call

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ListMaker />
+  </div>
+</template>
+
+<script>
+import ListMaker from './components/ListMaker.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ListMaker
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ListMaker.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p v-for="item in info" :key="item.id">
+      {{ item.id }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Info',
+  mounted () {
+    fetch(`http://www.mocky.io/v2/5ae1c5792d00004d009d7e5c`)
+      .then((res) => res.json())
+      .then((data) => {
+        this.info = data;
+      })
+  },
+  data() {
+    return {
+      info: []
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
