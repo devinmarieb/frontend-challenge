8ef671a0f596fe51b95a49756c4ee62ecc27b62a
Devin Beliveau <devinmarieb@gmail.com>
2020-09-23 17:27:39 +0200

    Fetch data on button click

--- src/components/ListMaker.vue
+++ src/components/ListMaker.vue
@@ -1,20 +1,27 @@
 <template>
   <div>
-    <p v-for="item in info" :key="item.id">
-      {{ item.id }}
-    </p>
+    <div v-for="item in info" :key="item.id">
+      <input type="checkbox" id="test" name="test" value="Test">
+      <p>{{ item.voice }}</p>
+      <p>{{ item.text }}</p>
+    </div>
+    <button v-on:click="fetchData()">+</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Info',
-  mounted () {
-    fetch(`http://www.mocky.io/v2/5ae1c5792d00004d009d7e5c`)
+  mounted () {},
+  methods: {
+    fetchData() {
+      fetch(`http://www.mocky.io/v2/5ae1c5792d00004d009d7e5c`)
       .then((res) => res.json())
       .then((data) => {
         this.info = data;
+        console.log(data)
       })
+    }
   },
   data() {
     return {
