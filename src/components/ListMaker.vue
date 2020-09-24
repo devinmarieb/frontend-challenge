69879d968b2ddbec9f35e31d31e742e49a46c7c3
Devin Beliveau <devinmarieb@gmail.com>
2020-09-24 19:04:35 +0200

    Add editable text

--- src/components/ListMaker.vue
+++ src/components/ListMaker.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <div v-for="item in info" :key="item.id">
-      <input type="checkbox" id="test" name="test" value="Test">
-      <p>{{ item.voice }}</p>
-      <p>{{ item.text }}</p>
+    <button v-on:click="fetchData()">download</button>
+
+    <div class="text-items" v-for="(item, index) in info" :key="item.id">
+      <input type="checkbox" id="test" name="test" value="Test"/>
+      <textarea v-model="item.voice" :placeholder="item.voice"></textarea>
+      <textarea v-model="item.text" :placeholder="item.text"></textarea>
+      <button v-on:click="deleteData(index)">delete</button>
     </div>
-    <button v-on:click="fetchData()">+</button>
+
+    <button v-on:click="addRow()">+</button>
+    <button v-on:click="uploadData()">upload</button>
   </div>
 </template>
 
@@ -15,21 +20,24 @@ export default {
   mounted () {},
   methods: {
     fetchData() {
-      fetch(`http://www.mocky.io/v2/5ae1c5792d00004d009d7e5c`)
+      fetch('http://www.mocky.io/v2/5ae1c5792d00004d009d7e5c')
       .then((res) => res.json())
-      .then((data) => {
-        this.info = data;
-        console.log(data)
-      })
-    }
+      .then((data) => this.info = data)
+    },
+    addRow() { this.info.push({voice: '', text: ''}) },
+    deleteData(index) { this.info.splice(index, 1) },
   },
   data() {
     return {
-      info: []
+      info: null,
+      range: 0
     }
   }
 }
 </script>
 
 <style scoped>
+  .text-items:hover {
+    border: 2px solid red;
+  }
 </style>
